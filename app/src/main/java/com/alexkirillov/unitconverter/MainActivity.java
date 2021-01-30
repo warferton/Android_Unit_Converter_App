89e2a56de38ad7f1ff9f0dab3610062bf9e869cd
warferton <warferton45@gmail.com>
2021-01-31 02:18:47 +0500

    Initial commit.  Only concept design is done.

---
+++ app/src/main/java/com/alexkirillov/unitconverter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.alexkirillov.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner_one = (Spinner) findViewById(R.id.unitSpinnerOne);
+        Spinner spinner_two = (Spinner) findViewById(R.id.unitSpinnerTwo);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.units_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner_one.setAdapter(adapter);
+        spinner_two.setAdapter(adapter);
+
+
+    }
+
+
+}
